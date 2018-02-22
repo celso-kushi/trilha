c78e9b4472b46e8b64eab3bf79d58fa1f2596b44
hiro <hiro@localhost.localdomain>
2018-02-21 22:05:08 -0300

    Configuração inicial Hibernate

--- trilha-ejb/src/main/java/br/com/kushi/financeiro/dao/FinanceiroDAO.java
+++ trilha-ejb/src/main/java/br/com/kushi/financeiro/dao/FinanceiroDAO.java
@@ -12,6 +12,11 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.ejb.EJBContext;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -19,80 +24,42 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class FinanceiroDAO {
-    
+
     @Resource
     EJBContext context;
-    
+
+    @PersistenceContext(unitName = "trilha")
+    private EntityManager entity;
+
+    public FinanceiroDAO() {
+    }
+
     public List<Lancamento> obterLancamentos() throws Exception {
-        
-        Connection conn = null;
-        Statement statement = null;
-        ResultSet rs = null;
 
-        List<Lancamento> lancamentos = new ArrayList<Lancamento>();
-        Lancamento lancamento = null;
         try {
-
-            conn = BancoDados.conectar();
-
-            statement = conn.createStatement();
-            rs = statement.executeQuery("SELECT * FROM financeiro");
-            
-            while (rs.next()) {
-                lancamento = new Lancamento(rs.getInt("id"), rs.getString("nome"), rs.getDate("data"), rs.getDouble("valor"), rs.getInt("tipo"));
-                lancamentos.add(lancamento);
-            }
-            
-            return lancamentos;
-        
-        } finally {
-            if (rs != null) rs.close();
-            if (statement != null) statement.close();
-            if (conn != null)   conn.close();
+            return entity.createQuery("SELECT l FROM Lancamento l", Lancamento.class).getResultList();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
         }
     }
 
-    
     public Integer inserir(Lancamento lancamento) throws Exception {
-        
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
 
         try {
 
-            conn = BancoDados.conectar();
+            lancamento = entity.merge(lancamento);            
+            entity.flush();
+            
+            return lancamento.getId();
 
-            StringBuilder sb = new StringBuilder();
-            
-            sb.append("INSERT INTO financeiro (nome, data, valor, tipo) values (?,?,?,?)");
-
-            ps = conn.prepareStatement(sb.toString());
-            ps.setString(1, lancamento.getNome());
-            ps.setDate(2, new Date(System.currentTimeMillis()));
-            ps.setDouble(3, lancamento.getValor());
-            ps.setInt(4, lancamento.getTipo());
-            
-            ps.executeUpdate();
-            
-            ps = conn.prepareStatement("SELECT CASE WHEN MAX(id) IS NULL THEN 0 ELSE MAX(id) END as id FROM financeiro");
-            rs = ps.executeQuery();
-            rs.next();
-            
-            return rs.getInt("id");
-            
         } catch (Exception e) {
             context.setRollbackOnly();
-            throw e;
-        } finally {
-            if (rs != null) rs.close();
-            if (ps != null) ps.close();
-            if (conn != null)   conn.close();
-        }
+            throw new Exception(e.getMessage());
+        } 
     }
 
     public Lancamento alterar(Lancamento lancamento) throws Exception {
-        
+
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -102,7 +69,7 @@ public class FinanceiroDAO {
             conn = BancoDados.conectar();
 
             StringBuilder sb = new StringBuilder();
-            
+
             sb.append("UPDATE financeiro SET nome = ?, data = ?, valor = ?, tipo = ? WHERE id = ?");
 
             ps = conn.prepareStatement(sb.toString());
@@ -111,23 +78,29 @@ public class FinanceiroDAO {
             ps.setDouble(3, lancamento.getValor());
             ps.setInt(4, lancamento.getTipo());
             ps.setInt(5, lancamento.getId());
-            
+
             ps.executeUpdate();
-            
+
             return obtemUnico(lancamento.getId());
-        
+
         } catch (Exception e) {
             context.setRollbackOnly();
             throw e;
         } finally {
-            if (rs != null) rs.close();
-            if (ps != null) ps.close();
-            if (conn != null)   conn.close();
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
     }
-    
+
     public Boolean excluir(Lancamento lancamento) throws Exception {
-        
+
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -137,60 +110,72 @@ public class FinanceiroDAO {
             conn = BancoDados.conectar();
 
             StringBuilder sb = new StringBuilder();
-            
+
             sb.append("DELETE FROM financeiro WHERE id = ?");
 
             ps = conn.prepareStatement(sb.toString());
             ps.setInt(1, lancamento.getId());
-            
+
             return (ps.executeUpdate() > 0);
-        
+
         } catch (Exception e) {
             context.setRollbackOnly();
             throw e;
         } finally {
-            if (rs != null) rs.close();
-            if (ps != null) ps.close();
-            if (conn != null)   conn.close();
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
     }
-    
+
     public Lancamento obtemUnico(int id) throws Exception {
-        
+
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        
+
         Lancamento lancamento = null;
-        
+
         try {
 
             conn = BancoDados.conectar();
 
             StringBuilder sb = new StringBuilder();
-            
+
             sb.append("SELECT * FROM financeiro WHERE id = ?");
 
             ps = conn.prepareStatement(sb.toString());
             ps.setInt(1, id);
-            
+
             rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 lancamento = new Lancamento(rs.getInt("id"), rs.getString("nome"), rs.getDate("data"), rs.getDouble("valor"), rs.getInt("tipo"));
             }
-            
+
             return lancamento;
-            
+
         } finally {
-            if (rs != null) rs.close();
-            if (ps != null) ps.close();
-            if (conn != null)   conn.close();
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
     }
-    
+
     public List<Lancamento> obterLancamentos(java.util.Date dataInicial, java.util.Date dataFinal, String nome, Integer tipo) throws Exception {
-        
+
         Connection conn = null;
         ResultSet rs = null;
         Statement st = null;
@@ -203,44 +188,52 @@ public class FinanceiroDAO {
 
             StringBuilder sb = montarQuery(dataInicial, dataFinal, nome, tipo);
             st = conn.createStatement();
-            rs = st.executeQuery(sb.toString());              
-            
+            rs = st.executeQuery(sb.toString());
+
             while (rs.next()) {
                 lancamento = new Lancamento(rs.getInt("id"), rs.getString("nome"), rs.getDate("data"), rs.getDouble("valor"), rs.getInt("tipo"));
                 lancamentos.add(lancamento);
             }
-            
+
             return lancamentos;
-        
+
         } finally {
-            if (rs != null) rs.close();
-            if (st != null) st.close();
-            if (conn != null)   conn.close();
+            if (rs != null) {
+                rs.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
     }
-    
-    private StringBuilder montarQuery (java.util.Date dataInicial, java.util.Date dataFinal, String nome, Integer tipo) throws Exception {
-        
+
+    private StringBuilder montarQuery(java.util.Date dataInicial, java.util.Date dataFinal, String nome, Integer tipo) throws Exception {
+
         try {
             StringBuilder sb = new StringBuilder();
-            
+
             sb.append("SELECT * FROM financeiro WHERE 1 = 1");
-            
-            if (dataInicial != null && dataFinal != null) 
+
+            if (dataInicial != null && dataFinal != null) {
                 sb.append(" AND data >= '").append(new Date(dataInicial.getTime())).append("' AND data <= '").append(new Date(dataFinal.getTime())).append("'");
-            
-            
-            if (nome != null) 
+            }
+
+            if (nome != null) {
                 sb.append(" AND UPPER(nome) LIKE '%").append(nome.toUpperCase()).append("%'");
-            
-            if (tipo != null) 
+            }
+
+            if (tipo != null) {
                 sb.append(" AND tipo = ").append(tipo);
-            
+            }
+
             return sb;
-            
+
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
     }
-    
+
 }
--- trilha-ejb/src/main/java/br/com/kushi/financeiro/model/Lancamento.java
+++ trilha-ejb/src/main/java/br/com/kushi/financeiro/model/Lancamento.java
@@ -12,7 +12,9 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -21,12 +23,13 @@ import javax.persistence.TemporalType;
  * @author hiro
  */
 @Entity
+@Table(name = "lancamento")
 public class Lancamento implements Serializable {
     
     private static final long serialVersionUID = 1L;
 
-	@Id
-    @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     
     private String descricao;
